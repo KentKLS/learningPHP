25509141a28a3bca686d493369b8100f59621dbc
Quentin <kent.26780@gmail.com>
2023-05-25 14:19:44 +0200

    fix head in header and item

--- php/header.php
+++ php/header.php
@@ -1,3 +1,14 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/normalize.css">
+
+    <script src="https://kit.fontawesome.com/9b4d5c1e32.js" crossorigin="anonymous"></script>
+
+    <title>index</title>
+</head>
 
 <header>
         <nav class="headerNav">
