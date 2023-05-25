9f4be49ad3bea744b5be30ea335547775ba226ae
Quentin <kent.26780@gmail.com>
2023-05-25 13:24:53 +0200

    Included header.php and footer.php inside item.php

--- php/item.php
+++ php/item.php
@@ -1,8 +1,15 @@
+
+
+
 <?php 
+
+include'header.php';
+
 $name = "test";
 $price = "22$";
 $path = "test.com";
 
     echo "Nom: $name \n Prix: $price \n URL: $path";
 
+    include 'footer.php';
 ?>
