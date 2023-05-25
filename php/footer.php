cfae73bcd65ffdf3cdba72ec84f9a0c4a3c88605
Quentin <kent.26780@gmail.com>
2023-05-25 13:07:14 +0200

    added 2 .php file containing a header and a footer

---
+++ php/footer.php
@@ -0,0 +1,20 @@
+<footer>
+        <nav class="footerNav">
+            <a href="https://discordapp.com/users/765549201965711381" target="_blank">
+
+                <!-- icon font awesome discord  -->
+                <i class="fab fa-discord "></i></a>
+            <a href="https://www.linkedin.com/in/quentin-sirjean-b09964254/" target="_blank">
+
+                <!-- icon font awesome linkedin  -->
+                <i class="fab fa-linkedin "></i></a>
+            <a href="https://www.instagram.com/notanartist780/" target="_blank">
+
+                <!-- icon font awesome instagram  -->
+                <i class="fab fa-instagram "></i></a>
+            <a href="https://github.com/KentKLS" target="_blank">
+
+                <!-- icon font awesome github  -->
+                <i class="fab fa-github "></i></a>
+        </nav>
+    </footer>
---
+++ php/header.php
@@ -0,0 +1,16 @@
+<header>
+        <nav class="headerNav">
+            <ul>
+                <li>
+                    <a href="#" target="_blank"> Navto Page 1</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank">Navto Page 2</a>
+                </li>
+                <li>
+                    <a href="#" target="_blank"> Navto Page 3</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
