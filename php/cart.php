5fd33d092bffb2665774513ed0baaa03cd1d686e
Quentin <kent.26780@gmail.com>
2023-05-26 16:38:50 +0200

    ajout d'un tableau

---
+++ php/cart.php
@@ -0,0 +1,29 @@
+<?php
+include "./item-list.php";
+?>
+<div class="tableContainer">
+    <table>
+        <tr>
+            <th>Produit</th>
+            <th>Prix Unitaire</th>
+            <th>Quantité</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>Nom</td>
+            <td>Prix</td>
+            <td>Quantité</td>
+            <td>Total</td>
+        </tr>
+        <tr>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>Test</td>
+        </tr>
+    </table>
+
+</div>
