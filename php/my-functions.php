39cc18ac9706ef07f09745cb041c5e740cb9e414
Quentin <kent.26780@gmail.com>
2023-05-26 15:21:34 +0200

    added var VAT to priceExcludingVAT

--- php/my-functions.php
+++ php/my-functions.php
@@ -1,17 +1,25 @@
 <?php
 
 
-function formatPrice($price){
+function formatPrice($price)
+{
 
-    echo number_format($price/100,2)," €";
+    echo number_format($price / 100, 2), " €";
 }
 
-function priceExcludingVAT($priceTTC) {
-    $priceHT = (100*$priceTTC)/(100+20);    
+function priceExcludingVAT($priceTTC, $VAT = 20)
+{
+    
+    $priceHT = (100 * $priceTTC) / (100 + $VAT);
     return $priceHT;
 }
 
-function discountedPrice($price, $discount){
-    $discountedPrice = $price - ($price * $discount/100 );
-    return $discountedPrice;
+function discountedPrice($price, $discount)
+{
+    if ($discount > 0) {
+        $discountedPrice = $price - ($price * $discount / 100);
+        return $discountedPrice;
+    }
+
+return $price;
 }
