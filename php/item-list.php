ca3a58cbbf81181d0db9775f1c425e4ed5824c31
Quentin <kent.26780@gmail.com>
2023-05-26 16:38:13 +0200

    Added 2 function that initialize my item list and help get item from the list

---
+++ php/item-list.php
@@ -0,0 +1,38 @@
+<?php
+
+function getProducts() {
+    return [
+        "iPhone" => [
+            "name" => 'iPhone',
+            "price" => 45000,
+            "weight" => 200,
+            "discount" => 10,
+            "picture_url" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1663611329492"
+        ],
+        "iPad" => [
+            "name" => 'iPad',
+            "price" => 575000,
+            "weight" => 300,
+            "discount" => 20,
+            "picture_url" => "https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        ],
+        "iMac" => [
+            "name" => 'iMac',
+            "price" => 750000,
+            "weight" => 250,
+            "discount" => 15,
+            "picture_url" => "https://www.apple.com/v/macbook-pro-14-and-16/e/images/overview/performance/choose_size__b11uc4j8f36u_large.jpg"
+        ],
+    ];
+}
+
+
+function getProduct($productName) {
+    $products = getProducts();
+
+    if ( ! isset($products[$productName]) )  {
+        throw new Error("Le produit n'existe pas");
+    }
+
+    return $products[$productName];
+}
