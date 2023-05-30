b75fa25c014c5f2686ce236fa1510302f84d9164
Quentin <kent.26780@gmail.com>
2023-05-30 11:39:19 +0200

    modified form

--- php/multidimensional-catalog.php
+++ php/multidimensional-catalog.php
@@ -1,75 +1,38 @@
 <?php
 include './my-functions.php';
-include "./item-list.php"
+include "./item-list.php";
+$products = getProducts();
 ?>
 
+<?php foreach ($products as $product => $value):
+        
+?>
 <div>
-    <h3> <?= $products["iPhone"]['name'] ?></h3>
+    <h3> <?= $products[$product]['name'] ?></h3>
     <p>
-        Prix : <?php formatPrice($products["iPhone"]['price'])  ?>
+        Prix : <?php formatPrice($products[$product]['price'])  ?>
     </p>
     <p>
-        Poids : <?= $products["iPhone"]['weight']  ?>g
+        Poids : <?= $products[$product]['weight']  ?>g
     </p>
     <p>
-        Price after <?= $products["iPhone"]['discount']   ?>
+        Price after <?= $products[$product]['discount']   ?>
         % discount :
         <?php
-        $discountedPrice =  discountedPrice($products["iPhone"]['price'], $products["iPhone"]['discount']);
+        $discountedPrice =  discountedPrice($products[$product]['price'], $products[$product]['discount']);
         formatPrice($discountedPrice);
         ?>
     </p>
+
     <form action="cart.php" method="POST">
 
-        <label for="iPhoneOrderedNumber">Quantité :</label>
-        <input type="number" name="iPhoneOrderedNumber">
+        <label for="numberOrdered">Quantité :</label>
+        <input type="hidden" name="product" value="<?php echo $products[$product]["name"]?>">
+        <input type="number" min="1" name="numberOrdered">
         <button class="buyButton">Achetez cet article</button>
         
     </form>
-    <img src='<?= $products["iPhone"]['picture_url'] ?>'>
+    <img src='<?= $products[$product]['picture_url'] ?>'>
 
 </div>
-<div>
-    <h3> <?= $products["iPad"]['name'] ?></h3>
-    <p>
-        Prix : <?php formatPrice($products["iPad"]['price']) ?>
-    </p>
-    <p>
-        Poids : <?= $products["iPad"]['weight'] ?>g
-    </p>
-    <p>
-        Price after <?= $products["iPad"]['discount']   ?>% discount : <?php
-                                                                        $discountedPrice =  discountedPrice($products["iPad"]['price'], $products["iPad"]['discount']);
-                                                                        formatPrice($discountedPrice) ?>
-    </p>
-    <form action="cart.php" method="POST">
-        <label for="iPadOrderedNumber">Quantité :</label>
-        <input type="number" name="iPadOrderedNumber">
-        <button class="buyButton">Achetez cet article</button>
-        
-    </form>
-    <img src='<?= $products["iPad"]['picture_url'] ?>'>
-
-</div>
-<div>
-    <h3> <?= $products["iMac"]['name'] ?></h3>
-    <p>
-        Prix : <?php formatPrice($products["iMac"]['price'])   ?>
-    </p>
-    <p>
-        Poids : <?= $products["iMac"]['weight']   ?>g
-    </p>
-    <p>
-        Price after <?= $products["iMac"]['discount']   ?>% discount : <?php
-                                                                        $discountedPrice =  discountedPrice($products["iMac"]['price'], $products["iMac"]['discount']);
-                                                                        formatPrice($discountedPrice) ?>
-    </p>
-    <form action="cart.php" method="POST">
-
-        <label for="iMacOrderedNumber">Quantité :</label>
-        <input type="number" name="iMacOrderedNumber">
-        <button class="buyButton">Achetez cet article</button>
-        
-    </form>
-    <img src='<?= $products["iMac"]['picture_url']  ?>'>
-</div>
+<?php endforeach ?>
