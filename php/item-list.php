5a58b168bf750b054a1a824408e9fe1372b7c8ae
Quentin <kent.26780@gmail.com>
2023-06-07 14:46:54 +0200

    changes to adapt to database usage

--- php/item-list.php
+++ php/item-list.php
@@ -1,41 +1,23 @@
 <?php
+include "./database.php";
 
-function getProducts() {
-    return [
-        "iPhone" => [
-            "name" => 'iPhone',
-            "price" => 45000,
-            "weight" => 200,
-            "discount" => 10,
-            "VAT"=> 20,
-            "picture_url" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1663611329492"
-        ],
-        "iPad" => [
-            "name" => 'iPad',
-            "price" => 575000,
-            "weight" => 300,
-            "discount" => 20,
-            "VAT"=> 20,
-            "picture_url" => "https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ],
-        "iMac" => [
-            "name" => 'iMac',
-            "price" => 750000,
-            "weight" => 250,
-            "discount" => 15,
-            "VAT"=> 20,
-            "picture_url" => "https://www.apple.com/v/macbook-pro-14-and-16/e/images/overview/performance/choose_size__b11uc4j8f36u_large.jpg"
-        ],
-    ];
+function getProductList($db)
+{
+    
+    $getProductList = $db->prepare(
+        "SELECT *
+        FROM products"
+    );
+    $getProductList->execute();
+    $productList = $getProductList->fetchAll($mode = PDO::FETCH_ASSOC);
+    return $productList;
 }
 
 
-function getProduct($productName) {
-    $products = getProducts();
+function getProduct($productName,$db) {
+    $products = getProductList($db);
 
-    if ( ! isset($products[$productName]) )  {
-        throw new Error("Le produit n'existe pas");
-    }
+
 
     return $products[$productName];
 }
--- php/multidimensional-catalog.php
+++ php/multidimensional-catalog.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 include "./header.php";
-include './my-functions.php';
-include "./item-list.php";
-$products = getProducts();
+include "./my-functions.php";
+$products = getProductList($db);
+$orders = getOrdersList($db);
+
 ?>
 <form action="cart.php" method="POST">
     <section class="mainContainer">
@@ -16,19 +17,19 @@ $products = getProducts();
 
                 <div class="productCard">
                     <div class="cardImgContainer">
-                        <img src='<?= $products[$product]['picture_url'] ?>'>
+                        <img src='<?= $products[$product]['product_imgURL'] ?>'>
                     </div>
-                    <h3><?= $products[$product]['name'] ?></h3>
-                    <p> Prix : <?php formatPrice($products[$product]['price'])  ?></p>
-                    <p>Poids : <?= $products[$product]['weight']  ?>g</p>
-                    <p>Price after <?= $products[$product]['discount']   ?>% discount :
+                    <h3><?= $products[$product]['product_name'] ?> </h3>
+                    <p> Prix : <?php formatPrice($products[$product]['product_price'])  ?></p>
+                    <p>Poids : <?= $products[$product]['product_weight']  ?>g</p>
+                    <p>Price after <?= 0   ?>% discount :
                         <?php
-                        $discountedPrice =  discountedPrice($products[$product]['price'], $products[$product]['discount']);
+                        $discountedPrice =  discountedPrice($products[$product]['product_price'], 0);
                         formatPrice($discountedPrice);
                         ?>
                     </p>
                     <label for="numberOrdered[]">Quantité :</label>
-                    <input type="hidden" name="product[]" value="<?php echo $products[$product]["name"] ?>">
+                    <input type="hidden" name="product[]" value="<?php echo $products[$product]["product_name"] ?>">
                     <input type="number" class="numberInput" value="<?=$_SESSION["numberOrdered"][$product] ??  0 ?>" min="0" max="99" name="numberOrdered[<?= $product ?>]">
                 </div>
 
