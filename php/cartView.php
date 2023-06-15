78e274c35429820b92ad51c740c4262f98e02871
Quentin <kent.26780@gmail.com>
2023-06-15 14:56:06 +0200

    change to views"

--- php/cartView.php
+++ php/cartView.php
@@ -1,137 +1,95 @@
 <?php
-session_start();
-include "./header.php";
+require_once './database/Cart.php';
 require_once "./my-functions.php";
-
+require_once './database/product.php';
+include "./header.php";
 
 
 if (isset($_POST["product"]) && isset($_POST["numberOrdered"])) {
+    $_SESSION['cart']->updateCart($_POST["product"], $_POST["numberOrdered"]);
+}
+
+if (isset($_SESSION['cart'])) {
+    $cart = $_SESSION['cart']->getCart();
+    $cartProducts = getCartProducts($cart);
+}
+
+if(isset($_POST['deleteProduct'])){    
+    $_SESSION['cart']->deleteCart($_POST["product"]);
 }
 
 
-
-
-
+// var_dump($_SESSION['cart']->cart);
 
 
 ?>
 
 <div class="mainContainer contentIsCentered isFullPage">
 
-    <?php
-
-    if (!isset($_POST["emptyCart"])) :
-        if (!isset($_POST["orderPassed"])) :
-    ?>
 
 
+    <?php if (!empty($_SESSION['cart']->cart) ) : ?>
+        <div class="tableContainer">
+            <table>
+                <tr>
+                    <th>Produit</th>
+                    <th>Prix Unitaire</th>
+                    <th>Quantité</th>
+                    <th></th>
+                    <th>Total</th>
+                    <th></th>
+                </tr>
 
-            <div class="tableContainer">
-                <form action="" method="POST">
-                    <table>
-                        <tr>
-                            <th>Produit</th>
-                            <th>Prix Unitaire</th>
-                            <th>Quantité</th>
-                            <th>Total</th>
-                            <th></th>
-                        </tr>
-                        <?php
-                            displayCart($cart)
-                        ?>
-
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Total HT</td>
-                            <td> <?php echo formatPrice(priceExcludingVAT($totalPrice)) ?> </td>
-
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>TVA</td>
-                            <td> <?php echo 20 ?>%</td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td>Total TTC</td>
-                            <td><?php echo formatPrice($totalPrice) ?></td>
-                        </tr>
-
-                    </table>
-                    <button>Mettre à jour tableau</button>
+                <?php
+                $totalPrice =displayCart($_SESSION['cart'])
+                ?>
 
 
-            </div>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>Total HT</td>
+                    <td> <?php formatPrice(priceExcludingVAT($totalPrice)) ?> </td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>TVA</td>
+                    <td> <?= 20 ?>%</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>Total TTC</td>
+                    <td><?php formatPrice($totalPrice) ?></td>
+                </tr>
+
+            </table>
 
 
 
-
-            <div class="horizontalDiv">
-                <label for="transporter">Transporteur:</label>
-                <select name="transporter">
-                    <option value="laPoste">La Poste</option>
-                    <option value="chronoPost">ChronoPost</option>
-                </select>
-                <button>VALIDER</button>
-
-            </div>
-            <?php if (isset($_POST["transporter"])) : ?>
-                <table>
-                    <tr>
-
-                        <td>TRANSPORT</td>
-                        <td><?php echo formatPrice(priceTransporter($_POST["transporter"], $array["weight"] * $array["numberOrdered"], $array["price"])); ?></td>
-
-                    </tr>
-                    <tr>
-
-                        <td>Total TTC</td>
-                        <td><?php echo formatPrice(priceTransporter($_POST["transporter"], $array["weight"] * $array["numberOrdered"], $array["price"]) + $totalPrice)  ?></td>
-
-                    </tr>
-
-                </table>
-
-            <?php
-            endif;
-            ?>
-            </form>
-            <form action="" method="POST">
-                <button>Vidé le panier</button>
-                <input type="hidden" name="emptyCart" value="0">
-
-            </form>
-            <form action="" method="POST">
-                <button>Validé Commande</button>
-                <input type="hidden" name="orderPassed" value="0">
-
-            </form>
-</div>
-<?php else :
-           
-            $totalPrice = calculTotalPrice($productsArray);
-            $orderID = insertNewOrderAndReturnOrderID($db, $totalPrice);
-            foreach ($productsArray as $product) {
-                if ($product["numberOrdered"] != 0) {
-                    insertNewOrder_product($db, $product["numberOrdered"], $product["productID"], $orderID);
-                }
-            }
+        </div>
 
 
-?>
-    <div>
-        Commande Enregistrée
-    </div>
-<?php   endif; ?>
-<?php else :   ?>
+    <?php else : ?>
 
-    <div>
+
         Le panier est vide
-    </div>
-<?php endif; ?>
 
 
-<?php include "./footer.php"; ?>
+
+    <?php endif ?>
+
+</div>
+<?php
+// $totalPrice = calculTotalPrice($productsArray);
+// $orderID = insertNewOrderAndReturnOrderID($db, $totalPrice);
+// foreach ($productsArray as $product) {
+//     if ($product["numberOrdered"] != 0) {
+//         insertNewOrder_product($db, $product["numberOrdered"], $product["productID"], $orderID);
+//     }
+// }
+
+
+include "./footer.php"; ?>
--- php/catalogueView.php
+++ php/catalogueView.php
@@ -1,17 +1,29 @@
 <?php
-include './my-functions.php';
-include './database/catalogue.php';
+require_once './database/Cart.php';
+require_once './header.php';
+require_once './my-functions.php';
+require_once './database/catalogue.php';
 $catalog = new Catalog;
-include './header.php';
 
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = new Cart;
+}
 
+if (isset($_POST["product"])) {
+    $_SESSION['cart']->addToCart($_POST["product"]);
+}
+
+var_dump( $_SESSION['cart']->cart)
 ?>
 
 <section class="mainContainer">
     <div class="cardContainer">
+
         <?php displayCatalog($catalog) ?>
 
     </div>
+
+
 </section>
-    <?php
-    include './footer.php';
+<?php
+include './footer.php';
